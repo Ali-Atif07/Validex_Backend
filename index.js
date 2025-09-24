1a5b57adebcff80bda1c621d071b3913dbbb183d
Ali-Atif07 <encodedecode10@gmail.com>
2025-09-24 13:21:04 +0530

    third

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+app.get('/',(req))
+
 // Redis client setup
 let redisClient;
 const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
